153f97f7d5215198202520b7216948489adad0d3
emiliu <emilyliu10@gmail.com>
2016-07-09 00:57:17 +0000

    add line following to threads

--- 03-threads.py
+++ 03-threads.py
@@ -2,32 +2,30 @@ from PiStorms import PiStorms
 from HiTechnicColorV2 import HiTechnicColorV2
 from time import sleep
 from threading import Thread, Event
+from pid import PIDController
 import math
 
 print "running program"
 psm = PiStorms()
 hc = HiTechnicColorV2()
 psm.BBS2.activateCustomSensorI2C()
+angle_pid = PIDController(-25, 25, 590.0, 0.05, 0.0, 0.0)
 
 #exit variable will be used later to exit the program and return to PiStorms
 exit = False
 
 def follow(e):
-    speed = 20
-    threshold = 600
-    threshold_ = 20
-    scale = 5
+    speed = 8
     while not exit and not e.isSet():
         value = psm.BBS1.lightSensorNXT(True)
-        offset = math.copysign(1, value - threshold) * scale if abs(value - threshold) > threshold_ else 0
+        offset = angle_pid.calculate(value)
         psm.screen.clearScreen()
         psm.screen.termPrintln(str(value) + ' ' + str(offset))
-        psm.BBM1.setSpeed(speed - offset)
-        psm.BBM2.setSpeed(speed + offset)
-    psm.BAM1.float()
-    psm.BAM2.float()
+        psm.BBM1.setSpeed(speed + offset)
+        psm.BBM2.setSpeed(speed - offset)
+        sleep(0.05)
     psm.BBM1.float()
-    psm.BBM2.float() 
+    psm.BBM2.float()
 
 def search(e, f):
     RED = 7         # or 8 or 9
@@ -46,7 +44,8 @@ def search(e, f):
             elif not f.isSet():
                 f.set()
         elif color >= RED and color <= RED + 2:
-            e.set()
+            #e.set()
+            print 'e.set()'
         sleep(0.1)
         psm.led(1, 0, 0, 0)
 
@@ -55,15 +54,12 @@ found_bomb = Event()
 
 f_thread = Thread(target=follow, args=(entered_house,))
 f_thread.start()
-s_thread = Thread(target=search, args=(entered_house, found_bomb))
-s_thread.start()
+#s_thread = Thread(target=search, args=(entered_house, found_bomb))
+#s_thread.start()
 
-while(not exit):
+while not exit:
 
-    #line_follow()
-    #find_victims()
-
-    if(psm.isKeyPressed() == True): # if the GO button is pressed
+    if psm.isKeyPressed(): # if the GO button is pressed
         psm.screen.clearScreen()
         psm.screen.termPrintln("Exiting to menu")
         psm.led(1,0,0,0)    
