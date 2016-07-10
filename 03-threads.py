44ea69b91d1ad7cd4985b25c9ccdb902c02f6e86
emiliu <emilyliu10@gmail.com>
2016-07-09 21:09:15 -0400

    dead reckoning through house
    not tested

--- 03-threads.py
+++ 03-threads.py
@@ -6,31 +6,64 @@ from pid import PIDController
 import math
 
 print "running program"
+
 psm = PiStorms()
 hc = HiTechnicColorV2()
 psm.BBS2.activateCustomSensorI2C()
 angle_pid = PIDController(-25, 25, 590.0, 0.05, 0.0, 0.0)
-
-#exit variable will be used later to exit the program and return to PiStorms
 exit = False
 
-def follow(e, f):
-    speed = 8
+
+def toDegs(dist, dia):
+    return int(float(dist) / dia * 360)
+
+def follow(e, f, g):
+    FOLLOW_SPEED = 8
+    SEARCH_SPEED = 80
+    WHEEL_DIA = 3.0 # in
+    ROBOT_WID = 6.0 # in
     while not exit and not e.isSet():
         value = psm.BBS1.lightSensorNXT(True)
         offset = angle_pid.calculate(value)
         #psm.screen.clearScreen()
         #psm.screen.termPrintln(str(value) + ' ' + str(offset))
-        psm.BBM1.setSpeed(speed + offset)
-        psm.BBM2.setSpeed(speed - offset)
+        psm.BBM1.setSpeed(FOLLOW_SPEED + offset)
+        psm.BBM2.setSpeed(FOLLOW_SPEED - offset)
         sleep(0.1)
-    while not exit and not f.isSet():
-        psm.BBM1.setSpeedSync(20)
+    if not exit and not f.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.5)
+        psm.BBM1.brakeSync()
+        psm.BBM1.runDegs(toDegs(ROBOT_WID * math.PI * 0.5, WHEEL_DIA), SEARCH_SPEED, True, True)
+    while not exit and not f.isSet() and not g.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
         sleep(0.1)
+    sleep(0.1)
+    if not exit and not f.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.5)
+        psm.BBM1.brakeSync()
+        psm.BBM1.runDegs(toDegs(ROBOT_WID * math.PI, WHEEL_DIA), SEARCH_SPEED, True, True)
+    while not exit and not f.isSet() and not g.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.1)
+    sleep(0.1)
+    if not exit and not f.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.5)
+        psm.BBM1.brakeSync()
+        psm.BBM1.runDegs(toDegs(ROBOT_WID * math.PI, WHEEL_DIA), SEARCH_SPEED, True, True)
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.5)
+        psm.BBM2.runDegs(toDegs(ROBOT_WID * math.PI * 0.5, WHEEL_DIA), SEARCH_SPEED, True, True)
+    while not exit and not f.isSet() and not g.isSet():
+        psm.BBM1.setSpeedSync(SEARCH_SPEED)
+        sleep(0.1)
+    sleep(0.1)
     psm.BBM1.float()
     psm.BBM2.float()
 
-def search(e, f):
+def search(e, f, g):
     RED = 7         # or 8 or 9
     GREEN = 4
     PURPLE = 17     # also black o.O
@@ -50,12 +83,17 @@ def search(e, f):
                 e.set()
                 psm.screen.termPrintln('e.set()')
                 print 'e.set()'
-        elif not f.isSet():
+        elif not f.isSet() and not g.isSet():
             if color > BLACK and color <= GREEN:
                 f.set()
+                g.set()
                 psm.led(1, 255, 0, 0)
                 psm.screen.termPrintln('f.set()')
                 print 'f.set()'
+            elif color >= PURPLE:
+                g.set()
+                sleep(0.1)
+                g.clear()
         sleep(0.1)
     psm.led(1, 0, 0, 0)
 
