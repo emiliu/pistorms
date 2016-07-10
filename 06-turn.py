06d19cecc2a333dbbf839b6f161907c48ba76242
emiliu <emilyliu10@gmail.com>
2016-07-10 03:51:54 +0000

    turn function!

---
+++ 06-turn.py
@@ -0,0 +1,28 @@
+from PiStorms import PiStorms
+'''
+from HiTechnicColorV2 import HiTechnicColorV2
+from time import sleep
+from threading import Thread, Event
+from pid import PIDController
+import math
+'''
+
+print "running program"
+
+SEARCH_SPEED = 50
+WHEEL_DIAMETER = 3.0 # in
+ROBOT_WIDTH = 8.0 # in
+
+psm = PiStorms()
+
+def turnDegs(degrees):
+    theta = int(abs(2.0 * ROBOT_WIDTH * degrees / WHEEL_DIAMETER))
+    print theta
+    if degrees > 0:
+        psm.BBM2.runDegs(theta, SEARCH_SPEED, True, False)
+    else:
+        psm.BBM1.runDegs(theta, SEARCH_SPEED, True, False)
+
+turnDegs(90)
+
+#psm.BBM1.runDegs(720, 50, True, False)
