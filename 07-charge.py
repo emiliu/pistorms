b8b65360eb10c487e85f035715252e867ae2c6b9
emiliu <emilyliu10@gmail.com>
2016-07-17 21:07:43 +0000

    simple sumo algo
    add stuff for partially raised ramp (test degrees)

--- 07-charge.py
+++ 07-charge.py
@@ -9,42 +9,58 @@ print 'running program'
 exit = False
 
 psm = PiStorms()
-if not psm.BAS1.presenceUSEV3():
+'''
+if not psm.BAS2.presenceUSEV3():
+    psm.screen.termPrintln('no ultrasonic sensor')
     print 'no ultrasonic sensor'
     exit = True
+'''
+
+EDGE_LIGHT = 650
+RAMP_DIST = 45
+RUSH_DIST = 300
+RAMP_ANGLE = 115    # actually 120, but with some leeway
+                    # also, negative is up
+
+
+def ramp(angle):
+    psm.BAM1.runDegs(angle, 80, True, True)
+    while psm.BAM1.isBusy():
+        pass
+    return
+
 
 def go():
     while not exit:
-        psm.screen.termPrintln('%d     %f' % (psm.BAS1.lightSensorNXT(True), psm.BBS1.distanceUSEV3in()))
-        '''
-        if psm.BAS1.lightSensorNXT(True) > 550:
+        psm.screen.termPrintln(str(psm.BAS1.lightSensorNXT(True)) + '   ' + str(psm.BAS2.distanceUSEV3in()) + '    ' + str(psm.BAM1.pos()))
+        if psm.BAS1.lightSensorNXT(True) > EDGE_LIGHT:
             psm.BBM1.setSpeed(100)
             psm.BBM2.setSpeed(-100)
-        elif psm.BBS1.distanceUSEV3in() < 30:
+            ramp(-1 * RAMP_ANGLE)
+        elif psm.BAS2.distanceUSEV3in() < RUSH_DIST:
             psm.BBM1.setSpeedSync(100)
+            ramp(RAMP_ANGLE)
         else:
             psm.BBM1.setSpeed(80)
             psm.BBM2.setSpeed(50)
-        '''
         sleep(0.1)
-    psm.BAM1.float()
-    psm.BAM2.float()
-
-def ramp():
-    pass
+    psm.BBM1.float()
+    psm.BBM2.float()
 
 
 if __name__ == '__main__':
+
+    psm.BAM1.resetPos()
 
     Thread(target = go).start()
 
     while not exit:
         sleep(0.05)
 
-    if psm.isKeyPressed():
-          psm.screen.clearScreen()
-          psm.screen.termPrintln("")
-          psm.screen.termPrintln("Exiting to menu")
-          psm.led(1,0,0,0)    
-          sleep(0.1)
-          exit = True
+        if psm.isKeyPressed():
+              psm.screen.clearScreen()
+              psm.screen.termPrintln("")
+              psm.screen.termPrintln("Exiting to menu")
+              psm.led(1,0,0,0)    
+              sleep(0.1)
+              exit = True
